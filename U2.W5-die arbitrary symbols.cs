7043bc22178f44da1f5881cf17f10d7c9e3734ab
Daniel Lewis <danlewis.ltd@gmail.com>
2014-07-28 16:47:09 +1200

    updated formatting

--- U2.W5-die arbitrary symbols.cs
+++ U2.W5-die arbitrary symbols.cs
@@ -1,28 +1,29 @@
 public class Die
 {
-List<String> sides;
-
-public Die (List<String> labels)
-{
-if (labels.Count < 1)
-{
-throw new ArgumentException ("What! you want to radomly select nothing?");
-}
-
-else {
-sides = labels;
-}
-}	
-
-public int Sides ()
-{
-return sides.Count;
-}
-
-public string Roll ()
-{
-Random random = new Random();
-int roll = random.Next(sides.Count);
-return sides[roll];
-}
+	List<String> sides;
+	
+	public Die (List<String> labels)
+	{
+		if (labels.Count < 1)
+		{
+			throw new ArgumentException ("What! you want to radomly select nothing?");
+		}
+	
+		else 
+		{
+			sides = labels;
+		}
+	}	
+	
+	public int Sides ()
+	{
+		return sides.Count;
+	}
+	
+	public string Roll ()
+	{
+		Random random = new Random();
+		int roll = random.Next(sides.Count);
+		return sides[roll];
+	}
 }
--- U2.W5-die numeric.cs
+++ U2.W5-die numeric.cs
@@ -1,28 +1,29 @@
 public class Die
 {
-int sides;
-
-public Die (int number)
-{
-if (number < 2)
-{
-throw new ArgumentException ("Thoust die bend the laws of my fantastical world!");
-}
-
-else {
-sides = number;
-}
-}	
-
-public int Sides ()
-{
-return sides;
-}
-
-public int Roll ()
-{
-Random rnd = new Random();
-int Roll = rnd.Next(1, sides);
-return Roll;
-}
+	int sides;
+	
+	public Die (int number)
+	{
+		if (number < 2)
+		{
+			throw new ArgumentException ("Thoust die bend the laws of my fantastical world!");
+		}
+	
+		else 
+		{
+			sides = number;
+		}
+	}	
+	
+	public int Sides ()
+	{
+		return sides;
+	}
+	
+	public int Roll ()
+	{
+		Random rnd = new Random();
+		int Roll = rnd.Next(1, sides);
+		return Roll;
+	}
 }
