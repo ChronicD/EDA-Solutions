f5fd181b9c52e01012c304021a835a59d2aa5603
Daniel Lewis <danlewis.ltd@gmail.com>
2014-07-29 21:36:10 +1200

    better formatting

--- U2.W4-harder calculate median.cs
+++ U2.W4-harder calculate median.cs
@@ -1,16 +1,16 @@
 public float Median(ArrayList array)
 {
-    int [] filter = (int[]) array.ToArray( typeof( int ) );
-Array.Sort(filter);
-int c = filter.Length;
-if (c % 2 == 0)
-{
-int temp = (filter[(c / 2)-1] + filter[c / 2]) ;
-       return (float) temp/(float)2;
-}
-else
-{
-       int temp = filter[c / 2];
-       return (float) temp;
-}
+    	int [] filter = (int[]) array.ToArray( typeof( int ) );
+	Array.Sort(filter);
+	int c = filter.Length;
+	if (c % 2 == 0)
+	{
+		int temp = (filter[(c / 2)-1] + filter[c / 2]) ;
+	       return (float) temp/(float)2;
+	}
+	else
+	{
+	       int temp = filter[c / 2];
+	       return (float) temp;
+	}
 }
